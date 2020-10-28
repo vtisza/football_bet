aa299f63f3ecbb1d440f9f676f5f279da2f48aae
vtisza <vtisza@gmail.com>
2020-10-28 11:56:02 +0100

    handle missing history

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from collections import Counter
 country_list = ["AUT", "DNK", "FIN","IRL","JPN","MEX","NOR","POL","ROU","RUS",
 "SWE","SWZ","USA"]
 
-st.title('Football app')
+st.title('Bet Master')
+st.header("Soccer Edition")
 
 country_select = st.selectbox(
     'Country',
@@ -22,8 +23,11 @@ fin_results = pd.read_csv(url)
 fin_results.Season = fin_results.Season.astype(str)
 fin_results.Season = fin_results.Season.apply(lambda x: x.split("/")[0]).astype(int)
 
-teams = fin_results.Home.unique()
-teams.sort()
+home_teams = fin_results.Home.unique()
+home_teams.sort()
+
+away_teams = fin_results.Away.unique()
+away_teams.sort()
 
 def normalize(dict_to_normal):
   total = sum(dict_to_normal.values())
@@ -33,12 +37,12 @@ def normalize(dict_to_normal):
 
 home = st.selectbox(
     'Home team',
-    teams,
+    home_teams,
     index=0)
 
 away = st.selectbox(
     'Away team',
-    teams,
+    away_teams,
     index=1)
 
 year_start = st.selectbox(
@@ -70,17 +74,20 @@ def run_simulation(fin_results, home, away, year_start, nr_simulations=100000):
 
     return simulations_df
 
-simulations_df = run_simulation(fin_results, home, away, year_start, nr_simulations=10000)
+if fin_results[(fin_results.Home==home) & (fin_results.Away==away) & (fin_results.Season>=year_start)].shape[0]==0:
+    st.header("Not enough history, please select other team combination.")
+else:
+    simulations_df = run_simulation(fin_results, home, away, year_start, nr_simulations=10000)
 
-st.header("Former games")
-st.dataframe(fin_results[(fin_results.Home==home) & (fin_results.Away==away) & (fin_results.Season>=year_start)])
+    st.header("Former games")
+    st.dataframe(fin_results[(fin_results.Home==home) & (fin_results.Away==away) & (fin_results.Season>=year_start)])
 
-st.header("Predicted outcomes")
-st.dataframe(simulations_df)
+    st.header("Predicted outcomes")
+    st.dataframe(simulations_df)
 
-st.header("Predicted odds")
-draw = 1/simulations_df[simulations_df.HG==simulations_df.AG].prob.sum()
-home_win = 1/simulations_df[simulations_df.HG>simulations_df.AG].prob.sum()
-away_win = 1/simulations_df[simulations_df.HG<simulations_df.AG].prob.sum()
+    st.header("Predicted odds")
+    draw = 1/simulations_df[simulations_df.HG==simulations_df.AG].prob.sum()
+    home_win = 1/simulations_df[simulations_df.HG>simulations_df.AG].prob.sum()
+    away_win = 1/simulations_df[simulations_df.HG<simulations_df.AG].prob.sum()
 
-st.write(f"{home_win:,.2f} - {draw:,.2f} - {away_win:,.2f}")
+    st.write(f"{home_win:,.2f} - {draw:,.2f} - {away_win:,.2f}")
